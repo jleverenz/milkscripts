2d7cf2e0ddab148dce5b8e201a611cc80e0e121d
Jeff Leverenz <jeff.leverenz@gmail.com>
2022-04-16 09:08:59 -0500

    Add doc to tag-archiver

--- tag-archiver.js
+++ tag-archiver.js
@@ -1,3 +1,33 @@
+// tag-archiver.js
+//
+// Delete tags not applied to any incomplete tasks.
+//
+// As part of the "archiving" process, the following will happen:
+//   1. A note is added to each *complete* tasks that had the archived tag.
+//   2. A single "report" task is created with a summary (and marked complete).
+//   3. Archived tags are deleted.
+//
+// For #1, any *complete* tasks that have the tag will have a new note attached
+// to it with the archived tag's name. For example if "projact-a" is archived,
+// the note will have the content "milkscript:archive-tag #project-a".
+//
+// For #2, the "report" task will be named "milkscript:tag-archiver - archive
+// report" and contain a note with a summary of tags archived (deleted) and the
+// number of tasks impacted.
+//
+// A special "input" task can be created to control this script. Name the task
+// "milkscript:tag-archiver" and include a single note. The note should contain
+// a list of regex or strings (one per line) indicating tags to ignore in the
+// archiving process. For example, the following note content would ignore tags
+// starting with a period, as well as the tag "waiting":
+//
+//    /\..+/
+//    waiting
+//
+// WARNING: This script has the potential to make significant changes to your
+// tasks and tags. Proceed with caution!
+
+
 function readInputTask(task) {
     const notes = task.getNotes();
     const keepLines = notes[0].getContent().split(/\n/).map(i => i.trim());
